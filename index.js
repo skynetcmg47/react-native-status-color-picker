d1892512fd86ac4d779f4950ecb53425302a6fd5
Nam Phuong <skynetcmg47@gmail.com>
2020-08-28 21:56:34 +0700

    add grid color picker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import ColorPicker from './lib/ColorPicker';
 import FullScreenColorStatus from './lib/FullScreenColorStatus';
 import StatusColorPicker from './lib/StatusColorPicker';
+import GridColorPicker from './lib/GridColorPicker';
 
-export { ColorPicker, FullScreenColorStatus, StatusColorPicker };
+export { ColorPicker, FullScreenColorStatus, StatusColorPicker, GridColorPicker };
